ac92bfbeeee800da2fb7fe4831fe3b49b87f835d
mt-xing <mt-xing@users.noreply.github.com>
2025-02-06 19:28:43 -0500

    Cleanup

--- l3/index.ts
+++ l3/index.ts
@@ -1,7 +1,6 @@
 import { getProgramFromCmdLine } from "../bril_shared/cfg.ts";
 import { jsonStringify } from "../bril_shared/io.ts";
-import { lvn, renameOverwrittenVariablesForProgram } from "./lvn.ts";
+import { lvn } from "./lvn.ts";
 
 const p = await getProgramFromCmdLine();
 console.log(jsonStringify(lvn(p)));
-// console.log(jsonStringify(renameOverwrittenVariablesForProgram(p)));
