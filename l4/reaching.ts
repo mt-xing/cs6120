9ff93b315c78ad39bcef97ea932d1d7986a39a08
mt-xing <mt-xing@users.noreply.github.com>
2025-02-12 21:15:45 -0500

    Use set instead of array

--- l4/reaching.ts
+++ l4/reaching.ts
@@ -1,36 +1,34 @@
 import { BasicBlock, BrilInstruction, CFG } from "../bril_shared/cfg.ts";
 import { dataflow } from "./dataflow.ts";
 
-function arrayEquals<T>(a: T[], b: T[]): boolean {
-    return a.length == b.length && a.every((x, i) => b[i] === x);
+function setEquals<T>(a: Set<T>, b: Set<T>): boolean {
+    return a.size == b.size && Array.from(a).every(x => b.has(x));
 }
 
-export function reachingDefs(cfg: CFG): Map<BasicBlock, Map<string, BrilInstruction[]>> {
-    return dataflow<Map<string, BrilInstruction[]>>(cfg, true, () => new Map(),
+export function reachingDefs(cfg: CFG): Map<BasicBlock, Map<string, Set<BrilInstruction>>> {
+    return dataflow<Map<string, Set<BrilInstruction>>>(cfg, true, () => new Map(),
         (b, ins) => {
             const set = new Map(ins);
             b.forEach((instr) => {
                 if (!("op" in instr)) { return; }
                 if (instr.dest) {
-                    set.set(instr.dest, [instr]);
+                    set.set(instr.dest, new Set([instr]));
                 }
             });
             return set;
         }, (vals) => {
-            const r = new Map<string, BrilInstruction[]>();
+            const r = new Map<string, Set<BrilInstruction>>();
             vals.forEach(m => {
                 m.forEach((entry, key) => {
                     const candidate = r.get(key);
                     if (candidate === undefined) {
                         r.set(key, entry);
                     } else {
-                        const set = new Set<BrilInstruction>(candidate);
-                        entry.forEach(x => set.add(x));
-                        r.set(key, Array.from(set));
+                        entry.forEach(x => candidate.add(x));
                     }
                 });
             });
             return r;
-        }, (a, b) => a.size === b.size && Array.from(a).every(x => arrayEquals(x[1], b.get(x[0]) ?? []))
+        }, (a, b) => a.size === b.size && Array.from(a).every(x => setEquals(x[1], b.get(x[0]) ?? new Set()))
     );
 }
