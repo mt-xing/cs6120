555f05ddb3a0032a08757d63418fadf751775679
mt-xing <mt-xing@users.noreply.github.com>
2025-03-06 10:09:23 -0500

    easier to read random names

--- bril_shared/newName.ts
+++ bril_shared/newName.ts
@@ -2,5 +2,5 @@ let nameCounter = 0;
 
 export function newName(oldName: string, sep?: string) {
     const s = sep ?? ":"
-    return `${oldName}${s}${nameCounter++}${s}${Math.random().toString(36).substring(2)}`
+    return `${oldName}${s}${nameCounter++}${s}${s}${s}${s}${s}${Math.random().toString(36).substring(2)}`
 }
