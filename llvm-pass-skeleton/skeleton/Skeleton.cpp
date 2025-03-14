58cc9c0888d808a6da24020e019eac66fd804cda
Ubuntu <mtxing@llvm-test.wh40ymoytbzurpe5f3euorv23c.cx.internal.cloudapp.net>
2025-03-14 02:01:56 +0000

    Basic pass

--- llvm-pass-skeleton/skeleton/Skeleton.cpp
+++ llvm-pass-skeleton/skeleton/Skeleton.cpp
@@ -4,57 +4,107 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 
+constexpr bool DEBUG = false;
+
 using namespace llvm;
 
 namespace {
 
 struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+        bool changed = false;
         for (auto &F : M) {
-            errs() << "I saw a function called " << F.getName() << "!\n";
-            errs() << "Function body:\n";
-            F.print(errs());
+            if (DEBUG) {
+                errs() << "I saw a function called " << F.getName() << "\n";
+                errs() << "Function body:\n";
+                F.print(errs());
+            }
 
             for (auto &B : F) {
-                errs() << "Basic block:\n";
-                B.print(errs());
+                if (DEBUG) {
+                    errs() << "Basic block:\n";
+                    B.print(errs());
+                }
 
                 for (auto &I : B) {
-                    errs() << "Instruction: ";
-                    I.print(errs(), true);
-                    errs() << "\n";
+                    if (DEBUG) {
+                        errs() << "Instruction: ";
+                        I.print(errs(), true);
+                        errs() << "\n";
+                    }
 
-                    // if I is an add instruction, change to mul
+                    // if I is a mul by 2^n instruction, change to <<n
                     if (auto *BO = dyn_cast<BinaryOperator>(&I)) {
-                        errs() << "Found Add: ";
-                        BO->print(errs());
+                        if (DEBUG) {
+                            errs() << "Op: " << BO->getOpcode() << "\n";
+                        }
 
-                        IRBuilder<> builder(BO);
-                        Value* lhs = BO->getOperand(0);
-                        Value* rhs = BO->getOperand(1);
+                        if (BO->getOpcode() == Instruction::BinaryOps::Mul) {
+                            if (DEBUG) {
+                                errs() << "Found Mul: ";
+                                BO->print(errs());
+                            }
 
-                        errs() << "\nLHS and RHS: \n";
-                        lhs->print(errs());
-                        errs() << "\n";
-                        rhs->print(errs());
-                        errs() << "\n";
+                            Value* lhs = BO->getOperand(0);
+                            Value* rhs = BO->getOperand(1);
 
-                        Value* mul = builder.CreateMul(lhs, rhs);
+                            if (DEBUG) {
+                                errs() << "\nLHS and RHS: \n";
+                                lhs->print(errs());
+                                errs() << "\n";
+                                rhs->print(errs());
+                                errs() << "\n";
 
-                        errs() << "New builder: \n";
-                        mul->print(errs());
-                        errs() << "\n";
+                                errs() << "Type: " << rhs->getType()->getTypeID() << "\n";
 
-                        for (auto &U : BO->uses()) {
-                            User* user = U.getUser();
-                            user->setOperand(U.getOperandNo(), mul);
+                                errs() << rhs->getValueName() << "\n";
+                            }
+
+                            if (auto *rhsVal = dyn_cast<ConstantInt>(rhs)) {
+                                if (DEBUG) {
+                                    errs() << "FOUND INTEGER\n";
+                                }
+                                int64_t r = rhsVal->getSExtValue();
+                                if (DEBUG) {
+                                    errs() << r << "\n";
+                                }
+
+                                double log2root = log2(r);
+                                
+                                if (DEBUG) {
+                                    errs() << log2root << "\n";
+                                }
+
+                                if (floor(log2root) == log2root && log2root > 0) {
+                                    if (DEBUG) {
+                                        errs() << "FOUND SHIFT CANDIDATE\n";
+                                    }
+
+                                    uint64_t rhsOfShift = static_cast<uint64_t>(log2root);
+
+                                    IRBuilder<> builder(BO);
+                                    Value* shl = builder.CreateShl(lhs, rhsOfShift);
+        
+                                    if (DEBUG) {
+                                        errs() << "New builder: \n";
+                                        shl->print(errs());
+                                        errs() << "\n";
+                                    }
+        
+                                    for (auto &U : BO->uses()) {
+                                        User* user = U.getUser();
+                                        user->setOperand(U.getOperandNo(), shl);
+
+                                        changed = true;
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
         }
-        // return PreservedAnalyses::all();
-        return PreservedAnalyses::none();
+        return changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
     };
 };
 
--- llvm-pass-skeleton/test.c
+++ llvm-pass-skeleton/test.c
@@ -3,5 +3,13 @@ int main() {
 }
 
 int add(int a, int b) {
+    int c1 = a * 2;
+    int c2 = a * 3;
+    int c3 = a * 4;
+    int c4 = a * 5;
+    int c5 = a * 6;
+    int c6 = a * 7;
+    int c7 = a * 8;
+    int c8 = a * 9;
     return a + b;
 }
